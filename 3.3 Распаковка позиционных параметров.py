506045be2556504f8d9b53195636a73eb4eed41c
Qvait <brylov.1996@mail.ru>
2024-10-02 15:03:02 +0500

    Delete 3.3 Распаковка позиционных параметров.py

--- 3.3 Распаковка позиционных параметров.py
+++
@@ -1,8 +0,0 @@
-def print_params(a = 1, b = 'строка', c = True):
-    print(a, b, c)
-values_list = [24.4, "Dibka", True]
-values_dict = {"a" : 25.3 , "b" : "Biba" , "c" : True}
-print_params(*values_list)
-print_params(**values_dict)
-values_list_2 = [22.1, "Giga"]
-print_params(*values_list_2, 42)

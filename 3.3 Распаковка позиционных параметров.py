6dd29f6f38e06d857ffb055c22e3c008d1cb63e1
Qvait <brylov.1996@mail.ru>
2024-10-02 15:03:17 +0500

    Add files via upload

---
+++ 3.3 Распаковка позиционных параметров.py
@@ -0,0 +1,10 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+values_list = [24.4, "Dibka", True]
+values_dict = {"a" : 25.3 , "b" : "Biba" , "c" : True}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [22.1, "Giga"]
+print_params(*values_list_2, 42)
+print_params(b = 25)
+print_params(c = [1,2,3])
